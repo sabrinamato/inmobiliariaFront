50a087347be81a101f92553e367cb7b36ee4cee8
sabirnamato <sabir.mato00@gmail.com>
2023-02-16 21:34:32 -0300

    Componente home con componente lista(get)

--- proyecto-final/src/API/Rule_inmobiliaria.jsx
+++ proyecto-final/src/API/Rule_inmobiliaria.jsx
@@ -1,1 +1,13 @@
 import API from "./Rule_API";
+
+export const propiedades = async () => {
+  let url = "/api/propiedades";
+  return await API.get(url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error.response.data.error || "Error precesando la solicitud";
+    });
+};
