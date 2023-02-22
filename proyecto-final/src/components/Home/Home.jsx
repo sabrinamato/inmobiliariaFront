ae57217cfc792f31df97d9370d54e23d08a0a712
sabirnamato <sabir.mato00@gmail.com>
2023-02-21 23:10:12 -0300

    portada

--- proyecto-final/src/components/Home/Home.jsx
+++ proyecto-final/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import List from "../List/List";
+import Portada from "../Portada/Portada";
 
 function Home() {
   return (
     <div>
+      <Portada />
       <List />
     </div>
   );
