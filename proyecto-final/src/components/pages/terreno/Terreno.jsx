3035939fb5590f5a0fac58294a65b54a2dfab826
Guzman <centuriong02@gmail.com>
2023-03-03 00:07:40 -0300

    cambios

--- proyecto-final/src/components/pages/terreno/Terreno.jsx
+++ proyecto-final/src/components/pages/terreno/Terreno.jsx
@@ -1,10 +1,10 @@
 
 
-const terreno = () => {
-    return (
+// const terreno = () => {
+//     return (
         
-        <h1>Hola desde el componente Terreno</h1>
-    );
-}
+//         <h1>Hola desde el componente Terreno</h1>
+//     );
+// }
 
-export default terreno;
+// export default terreno;
